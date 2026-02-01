d36c6b09d13fe269fd8664dc10bb4caf06fba851
dasakash26 <akashd2664@gmail.com>
2026-02-01 18:55:44 +0530

    feat(render): send rays into scene to render bg

--- include/vec3.h
+++ include/vec3.h
@@ -45,20 +45,20 @@ public:
 using Point3 = Vec3;
 
 // Utility fun
-inline std::ostream &operator<<(std::ostream out, const Vec3 &v) {
+inline std::ostream &operator<<(std::ostream &out, const Vec3 &v) {
   return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
 inline Vec3 operator+(const Vec3 &u, const Vec3 &v) {
-  return Vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], v.e[2] + v.e[2]);
+  return Vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
 inline Vec3 operator-(const Vec3 &u, const Vec3 &v) {
-  return Vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], v.e[2] - v.e[2]);
+  return Vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
 inline Vec3 operator*(const Vec3 &u, const Vec3 &v) {
-  return Vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], v.e[2] * v.e[2]);
+  return Vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
 inline Vec3 operator*(double t, const Vec3 &v) {
@@ -74,9 +74,11 @@ inline double dot(const Vec3 &u, const Vec3 &v) {
 }
 
 inline Vec3 cross(const Vec3 &u, const Vec3 &v) {
-  return Vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+  return Vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+              u.e[2] * v.e[0] - u.e[0] * v.e[2],
+              u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-inline Vec3 cap(const Vec3 &v) { return v / v.mod(); }
+inline Vec3 unit_vec(const Vec3 &v) { return v / v.mod(); }
 
 #endif // !VEC3_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,56 @@
 #include "color.h"
+#include "ray.h"
+#include "vec3.h"
 #include <iostream>
 
 using namespace std;
 
-int main() {
-  int img_w = 256, img_h = 256;
+Color ray_color(const Ray &r) {
+  Vec3 u_dir = unit_vec(r.direction());
+  auto a = (u_dir.y() + 1.0) * 0.5;
+  // LERP
+  auto res = (1.0 - a) * Color(1.0, 1.0, 1.0) + a * Color(0.5, 0.7, 1.0);
+  return res;
+}
 
-  // Render
-  // P3 means ascii
-  // then img w-h
-  // 255 is max color val
+int main() {
+
+  // Img
+  auto aspect_ratio = 2.0;
+  int img_w = 400;
+  int img_h = int(img_w / aspect_ratio);
+  img_h = (img_h) ? img_h : 1; // atleast 1
+
+  // Camera
+  auto focal_len = 1.0;
+  auto vh = 2.0;
+  auto vw = (double(img_w) / img_h) * vh; // real ar * vh
+  auto camera_center = Point3(0, 0, 0);
+
+  // Vectors: across the horizontal and down the vertical viewport edges
+  auto vu = Vec3(vw, 0, 0);
+  auto vv = Vec3(0, -vh, 0);
+
+  // Vector: delta from px to px
+  auto px_du = vu / img_w;
+  auto px_dv = vv / img_h;
+
+  // UL pixel
+  auto vp_upper_left = camera_center - Vec3(0, 0, focal_len) - vu / 2 - vv / 2;
+  auto px_00 = vp_upper_left + (px_du + px_dv) * 0.5; // center of ul px
+
   cout << "P3\n" << img_w << ' ' << img_h << "\n255\n";
 
   for (int j = 0; j < img_h; ++j) {
     clog << "\r>> Scanlines remaining: " << img_h - j - 1 << ' ' << flush;
     for (int i = 0; i < img_w; ++i) {
-      auto r = double(i) / (img_w - 1);
-      auto g = double(j) / (img_h - 1);
-      auto b = 0;
+      auto px_ij = px_00 + i * px_du + j * px_dv;
+      auto ray_dir = px_ij - camera_center;
 
-      Color pixel(r, g, b);
-      write_color(cout, pixel);
+      Ray r(camera_center, ray_dir);
+
+      Color px_clr = ray_color(r);
+      write_color(cout, px_clr);
     }
   }
 
